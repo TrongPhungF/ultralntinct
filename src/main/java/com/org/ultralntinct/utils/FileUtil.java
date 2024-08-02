4b9ca1e16b496282d665d78abe7183fe746bc27e
PhungHuynh <huynhtrongphungse@gmail.com>
2024-08-02 08:02:30 +0700

    add file util

---
+++ src/main/java/com/org/ultralntinct/utils/FileUtil.java
@@ -0,0 +1,15 @@
+package com.org.ultralntinct.utils;
+
+import jakarta.servlet.http.Part;
+
+public class FileUtil {
+    public static String getFileName(Part part) {
+        for (String content : part.getHeader("content-disposition").split(";")) {
+            if (content.trim().startsWith("filename")) {
+                return content.substring(content.indexOf('=') + 1).trim().replace("\"", "");
+            }
+        }
+        return null;
+    }
+
+}
