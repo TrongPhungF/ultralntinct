3469275c1199a74d3238787155238fdebf9102d8
PhungHuynh <huynhtrongphungse@gmail.com>
2024-08-04 09:31:56 +0700

    add comment java doc v1.1

--- src/main/java/com/org/ultralntinct/model/NhanVien.java
+++ src/main/java/com/org/ultralntinct/model/NhanVien.java
@@ -35,47 +35,61 @@ import lombok.Setter;
 @Table(name = "NhanVien")
 public class NhanVien implements Serializable {
 
+    /** The Constant serialVersionUID. */
     private static final long serialVersionUID = 1L;
 
+    /** The nhan vien no. */
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     @Column(name = "nhanVienNo")
     private Long nhanVienNo;
 
+    /** The ma nhan vien. */
     @Column(name = "maNhanVien", unique = true)
     private String maNhanVien;
 
+    /** The ten nhan vien. */
     @Column(name = "tenNhanVien")
     private String tenNhanVien;
 
+    /** The ho nhan vien. */
     @Column(name = "hoNhanVien")
     private String hoNhanVien;
 
+    /** The mat khau. */
     @Column(name = "matKhau")
     private String matKhau;
 
+    /** The so dien thoai. */
     @Column(name = "soDienThoai")
     private String soDienThoai;
 
+    /** The email. */
     @Column(name = "email")
     private String email;
 
+    /** The chuc vu. */
     @Column(name = "chucVu")
     private String chucVu;
 
+    /** The trang thai xoa. */
     @Column(name = "trangThaiXoa")
     private Boolean trangThaiXoa;
 
+    /** The thoi gian tao. */
     @Column(name = "thoiGianTao")
     private Date thoiGianTao;
 
+    /** The nguoi tao. */
     @Column(name = "nguoiTao")
     private String nguoiTao;
 
+    /** The nhan vien tao. */
     @ManyToOne
-    @JoinColumn(name = "nguoiTao", columnDefinition = "maNhanVien", insertable = false, updatable = false)
+    @JoinColumn(name = "nguoiTao", referencedColumnName = "maNhanVien", insertable = false, updatable = false)
     private NhanVien nhanVienTao;
 
+    /** The hoa don list. */
     @OneToMany(mappedBy = "nhanVien")
     private List<HoaDon> hoaDonList;
 }
