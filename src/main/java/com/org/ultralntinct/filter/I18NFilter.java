9b0831cf9e49f5a76a921318237b20e8118cf89d
PhungHuynh <huynhtrongphungse@gmail.com>
2024-08-04 19:10:28 +0700

    add commnet and muti language

---
+++ src/main/java/com/org/ultralntinct/filter/I18NFilter.java
@@ -0,0 +1,24 @@
+package com.org.ultralntinct.filter;
+
+import java.io.IOException;
+
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebFilter;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+@WebFilter("/*")
+public class I18NFilter implements HttpFilter {
+
+    @Override
+    public void doFilter(HttpServletRequest req, HttpServletResponse resp, FilterChain chain)
+            throws IOException, ServletException {
+        String lang = req.getParameter("lang");
+        if (lang != null) {
+            req.getSession().setAttribute("lang", lang);
+        }
+        chain.doFilter(req, resp);
+    }
+
+}
